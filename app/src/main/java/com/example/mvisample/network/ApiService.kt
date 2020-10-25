738057074aa06a56c431e8dfabf087e9a8cd4c62
brunohensel <bruno.hensel@gmail.com>
2020-10-25 11:03:57 +0100

    created generic api response class and a livedata call adapter(factory).

--- app/src/main/java/com/example/mvisample/network/ApiService.kt
+++ app/src/main/java/com/example/mvisample/network/ApiService.kt
@@ -1,14 +1,16 @@
 package com.example.mvisample.network
 
+import androidx.lifecycle.LiveData
 import com.example.mvisample.model.BLogPost
 import com.example.mvisample.model.User
+import com.example.mvisample.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
     @GET("placeholder/blogs")
-    fun fetchUser(): List<BLogPost>
+    fun fetchUser(): LiveData<GenericApiResponse<List<BLogPost>>>
 
     @GET("placeholder/user/{userId}")
-    fun fetchUser(@Path("userId") userId: String): User
+    fun fetchUser(@Path("userId") userId: String): LiveData<GenericApiResponse<User>>
 }
